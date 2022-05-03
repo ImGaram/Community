a33c8662c9da39baf3ad9d91dc8a32041a976bc1
imgaram <s21049@gsm.hs.kr>
2022-05-03 20:56:53 +0900

    django community 6: api interface

--- data/src/main/java/com/example/data/api/NbService.kt
+++ data/src/main/java/com/example/data/api/NbService.kt
@@ -2,12 +2,13 @@ package com.example.data.api
 
 import com.example.data.entity.request.SignInRequest
 import com.example.data.entity.response.SignInResponse
-import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface NbService {
     @POST("/User/")
-    fun signInService(@Body signInRequest: SignInRequest): Single<SignInResponse>
+    suspend fun signInService(
+        @Body body: SignInRequest
+    ): Response<SignInResponse>
 }
