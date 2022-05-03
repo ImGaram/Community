3b894ddc93f2d67e391f74a8df8bf005d93f216c
imgaram <s21049@gsm.hs.kr>
2022-05-03 20:58:32 +0900

    django community 6-3: mapper class

--- data/src/main/java/com/example/data/mapper/SignInMapper.kt
+++ data/src/main/java/com/example/data/mapper/SignInMapper.kt
@@ -2,6 +2,7 @@ package com.example.data.mapper
 
 import com.example.data.entity.response.SignInResponse
 import com.example.domain.model.DomainSignInResponse
+import retrofit2.Response
 
 object SignInMapper {
     // data -> domain
@@ -12,5 +13,18 @@ object SignInMapper {
             this.email,
             this.password
         )
+    }
+
+    fun toDomainResponse(
+        dataResponse: SignInResponse?
+    ): DomainSignInResponse? {
+        return if (dataResponse != null) {
+            DomainSignInResponse(
+                id = dataResponse.id,
+                email = dataResponse.email,
+                name = dataResponse.name,
+                password = dataResponse.password
+            )
+        } else dataResponse
     }
 }
