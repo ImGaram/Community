65e05e659b043ee8199e57e1ac2c901d660a636d
imgaram <s21049@gsm.hs.kr>
2022-05-03 20:58:55 +0900

    django community 6-4: dataSource class

--- data/src/main/java/com/example/data/repository/remote/SignInDataSource.kt
+++ data/src/main/java/com/example/data/repository/remote/SignInDataSource.kt
@@ -1,11 +1,10 @@
 package com.example.data.repository.remote
 
-import com.example.data.base.BaseSignInResponse
 import com.example.data.entity.request.SignInRequest
 import com.example.data.entity.response.SignInResponse
-import io.reactivex.rxjava3.core.Single
-import retrofit2.Response
 
 interface SignInDataSource {
-    suspend fun signUp(request: SignInRequest): Single<SignInResponse>
+    suspend fun signUp(
+        body: SignInRequest
+    ): SignInResponse?
 }
--- data/src/main/java/com/example/data/repository/remote/SignInDataSourceImpl.kt
+++ data/src/main/java/com/example/data/repository/remote/SignInDataSourceImpl.kt
@@ -1,19 +1,19 @@
 package com.example.data.repository.remote
 
-import com.example.data.base.BaseDataSource
 import com.example.data.api.NbService
-import com.example.data.base.BaseSignInResponse
+import com.example.data.base.BaseDataSource
 import com.example.data.entity.request.SignInRequest
 import com.example.data.entity.response.SignInResponse
-import io.reactivex.rxjava3.core.Single
-import retrofit2.Response
 import javax.inject.Inject
 
-class SignInDataSourceImpl @Inject constructor(override val service: NbService)
-    : BaseDataSource<NbService>(), SignInDataSource {
+class SignInDataSourceImpl @Inject constructor(
+    override val service: NbService
+    ): BaseDataSource<NbService>(), SignInDataSource {
     override suspend fun signUp(
-        request: SignInRequest
-    ): Single<SignInResponse> {
-        return service.signInService(request)
+        body: SignInRequest
+    ): SignInResponse? {
+        return safeApiCall {
+            service.signInService(body)
+        }?.body()
     }
 }
