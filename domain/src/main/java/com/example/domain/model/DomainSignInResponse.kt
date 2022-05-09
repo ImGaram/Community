419857361115981af196ae61a77ecb35211a2238
imgaram <s21049@gsm.hs.kr>
2022-05-09 20:34:02 +0900

    django community 7: 데이터 출력 response data

--- domain/src/main/java/com/example/domain/model/DomainSignInResponse.kt
+++ domain/src/main/java/com/example/domain/model/DomainSignInResponse.kt
@@ -6,3 +6,10 @@ data class DomainSignInResponse(
     val email: String,
     val password: String,
 )
+
+//interface DomainSignInResponse {
+//    val id: String
+//    val name: String
+//    val email: String
+//    val password: String
+//}
