0bbee1c63501536df496789bb3ba1d01ea504972
imgaram <s21049@gsm.hs.kr>
2022-05-10 22:43:45 +0900

    django community 8-7: login viewModel 파일

--- presentation/src/main/java/com/example/presentation/viewmodel/NbViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/NbViewModel.kt
@@ -4,8 +4,10 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.domain.model.DomainSignInResponse
-import com.example.domain.usecase.SignInUseCase
+import com.example.domain.model.user.DomainLoginResponse
+import com.example.domain.model.user.DomainSignInResponse
+import com.example.domain.usecase.login.LoginUseCase
+import com.example.domain.usecase.signin.SignInUseCase
 import com.example.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -13,17 +15,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class NbViewModel @Inject constructor(
-    private val signInUseCase: SignInUseCase
+    private val signInUseCase: SignInUseCase,
+    private val loginUseCase: LoginUseCase
 ): BaseViewModel() {
-
-    private val _apiCallResult = MutableLiveData<DomainSignInResponse>()
-    val apiCallResult: LiveData<DomainSignInResponse> = _apiCallResult
+    // 회원가입
+    private val _signInApiCallResult = MutableLiveData<DomainSignInResponse>()
+    val signInApiCallResult: LiveData<DomainSignInResponse> = _signInApiCallResult
     fun signInLogic(name: String, email: String, password: String) {
         viewModelScope.launch{
-            // TODO :: response json data 받기
             signInUseCase(name, email, password, viewModelScope) {
-                _apiCallResult.value = it
-                Log.d("SUCCESS", "_apiCallResult.value: ${_apiCallResult.value}")
+                _signInApiCallResult.value = it
+                Log.d("SUCCESS", "_signInApiCallResult.value: ${_signInApiCallResult.value}")
+            }
+        }
+    }
+
+    // 로그인
+    private val _loginApiCallResult = MutableLiveData<DomainLoginResponse>()
+    val loginApiCallResult: LiveData<DomainLoginResponse> = _loginApiCallResult
+    fun loginLogic(email: String, password: String) {
+        viewModelScope.launch {
+            loginUseCase(email, password, viewModelScope) {
+                _loginApiCallResult.value = it
+                Log.d("SUCCESS", "_loginApiCallResult.value: ${_loginApiCallResult.value}")
             }
         }
     }
