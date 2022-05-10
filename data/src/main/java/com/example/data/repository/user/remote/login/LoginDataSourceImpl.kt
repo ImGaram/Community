75ecffa560ca06295412c15657a4c6d42e1871ff
imgaram <s21049@gsm.hs.kr>
2022-05-10 22:37:40 +0900

    django community 8-1: login data source 파일

---
+++ data/src/main/java/com/example/data/repository/user/remote/login/LoginDataSourceImpl.kt
@@ -0,0 +1,17 @@
+package com.example.data.repository.user.remote.login
+
+import com.example.data.api.NbLoginService
+import com.example.data.base.BaseDataSource
+import com.example.data.entity.user.request.LoginRequest
+import com.example.data.entity.user.response.LoginResponse
+import javax.inject.Inject
+
+class LoginDataSourceImpl @Inject constructor(
+    override val service: NbLoginService
+) : BaseDataSource<NbLoginService>(), LoginDataSource{
+    override suspend fun login(user: LoginRequest): LoginResponse? {
+        return safeApiCall {
+            service.loginService(user)
+        }?.body()
+    }
+}
