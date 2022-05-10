0af5f30eee5209ae5f5cdb14fa453106b4d57437
imgaram <s21049@gsm.hs.kr>
2022-05-10 22:37:10 +0900

    django community 8-1: api request, response 파일

---
+++ data/src/main/java/com/example/data/entity/user/response/LoginResponse.kt
@@ -0,0 +1,10 @@
+package com.example.data.entity.user.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+    @SerializedName("id") var id: String,
+    @SerializedName("name") var name: String,
+    @SerializedName("email") var email: String,
+    @SerializedName("password") var password: String
+)
