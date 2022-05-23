b5b50899c0c13985408ef1d11c3e103e386d7719
imgaram <s21049@gsm.hs.kr>
2022-05-23 20:47:15 +0900

    django community 9-13: api service url 수정

--- data/src/main/java/com/example/data/api/NbUserInfoService.kt
+++ data/src/main/java/com/example/data/api/NbUserInfoService.kt
@@ -3,9 +3,10 @@ package com.example.data.api
 import com.example.data.entity.user.response.UserInfoResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface NbUserInfoService {
-    @GET("/User/{pk}")
-    fun getUser(@Query("pk") pk: Int): Response<UserInfoResponse>
+    @GET("/User/{id}")
+    suspend fun getUser(@Path("id") pk: Int): Response<UserInfoResponse>
 }
