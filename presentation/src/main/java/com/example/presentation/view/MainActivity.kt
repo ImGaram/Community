3f51e7fced51cc9c018be479fdc2ba5a91322d28
imgaram <s21049@gsm.hs.kr>
2022-05-23 20:49:07 +0900

    django community 9-13: activity bottom navigation 변경, bundle 데이터 삽입

--- presentation/src/main/java/com/example/presentation/view/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/MainActivity.kt
@@ -1,45 +1,56 @@
 package com.example.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import com.example.presentation.R
 import com.example.presentation.databinding.ActivityMainBinding
-import com.example.presentation.viewmodel.NbViewModel
+import com.example.presentation.view.fragment.FreeBoardFragment
+import com.example.presentation.view.fragment.InquiryFragment
+import com.example.presentation.view.fragment.NoticeFragment
+import com.example.presentation.view.fragment.StoryBoardFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var nav: BottomNavigationView
-    private val mainViewModel by viewModels<NbViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        nav = binding.bottomNavigationView
-        setBottomNavi(navHostFragment)
-        getUser()
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener(this)
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, FreeBoardFragment()).commit()
     }
 
-    private fun setBottomNavi(navHost: NavHostFragment) {
-        val navController = navHost.navController
-        nav.setupWithNavController(navController)
-    }
-
-    private fun getUser() {
-        val id = intent.getIntExtra("userId", 0)
-        mainViewModel.getUserLogic(id)
-        mainViewModel.userInfoApiCallResult.observe(this) {
-            Log.d("SUCCESS", "getUser it: $it")
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.freeBoard -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, FreeBoardFragment().apply {
+                    arguments = Bundle().apply {
+                        Log.d("SUCCESS", "onNavigationItemSelected intent extra: ${intent.getIntExtra("userId", 0)}")
+                        putInt("id", intent.getIntExtra("userId", 0))
+                    }
+                }).commitAllowingStateLoss()
+                return true
+            }
+            R.id.inquiry -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, InquiryFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.notice -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, NoticeFragment()).commitAllowingStateLoss()
+                return true
+            }
+            R.id.storyBoard -> {
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, StoryBoardFragment()).commitAllowingStateLoss()
+                return true
+            }
         }
+        return false
     }
 }
