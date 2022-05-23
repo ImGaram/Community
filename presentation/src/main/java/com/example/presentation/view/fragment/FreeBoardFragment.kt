76b857b33a96bebfd3f460c98cc12452601416e2
imgaram <s21049@gsm.hs.kr>
2022-05-23 20:49:30 +0900

    django community 9-13: fragment bundle 데이터 받기

--- presentation/src/main/java/com/example/presentation/view/fragment/FreeBoardFragment.kt
+++ presentation/src/main/java/com/example/presentation/view/fragment/FreeBoardFragment.kt
@@ -1,26 +1,47 @@
 package com.example.presentation.view.fragment
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.content.Intent
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.fragment.app.activityViewModels
 import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentFreeBoardBinding
+import com.example.presentation.view.login.LoginActivity
+import com.example.presentation.viewmodel.NbViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class FreeBoardFragment : Fragment() {
+@AndroidEntryPoint
+class FreeBoardFragment : BaseFragment<FragmentFreeBoardBinding>(R.layout.fragment_free_board), View.OnClickListener {
+    private val nbViewModel by activityViewModels<NbViewModel>()
+    override fun init() {
+        binding.free = this
+        viewSetting()
+        getUser()
+    }
 
-    private lateinit var binding: FragmentFreeBoardBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        binding = FragmentFreeBoardBinding.inflate(layoutInflater, container, false)
-        binding.goLoginText.setOnClickListener {
-            Navigation.findNavController(binding.root).navigate(R.id.action_freeBoardFragment_to_loginActivity)
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            binding.goLoginText.id -> startActivity(Intent(context, LoginActivity::class.java))
         }
+    }
 
-        return binding.root
+    private fun getUser() {
+        val id = arguments?.getInt("id")
+        Log.d("SUCCESS", "bundle id: $id")
+        if (id == null) {
+            Log.d("SUCCESS", "getUser: tlqkf")
+        } else {
+            nbViewModel.getUserLogic(id)
+            nbViewModel.userInfoApiCallResult.observe(viewLifecycleOwner) {
+                Log.d("SUCCESS", "getUser it: $it")
+            }
+        }
+    }
+
+    private fun viewSetting() {
+        binding.apply {
+            goLoginText.setOnClickListener(this@FreeBoardFragment)
+        }
     }
 }
