583daa8bd7a024aa902b503a4bf34a51b0be4ff6
imgaram <s21049@gsm.hs.kr>
2022-05-24 21:09:55 +0900

    django community 10-11: 유저 정보 fragment 추가

---
+++ presentation/src/main/java/com/example/presentation/view/user/UserInfoFragment.kt
@@ -0,0 +1,84 @@
+package com.example.presentation.view.user
+
+import android.util.Log
+import android.view.View
+import androidx.fragment.app.activityViewModels
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentUserInfoBinding
+import com.example.presentation.viewmodel.NbViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UserInfoFragment : BaseFragment<FragmentUserInfoBinding>(R.layout.fragment_user_info), View.OnClickListener {
+    private val nbViewModel by activityViewModels<NbViewModel>()
+
+    override fun init() {
+        binding.data = this
+        setUserInfo()
+        viewSetting()
+    }
+
+    private fun setUserInfo() {
+        binding.name.text = arguments?.getString("name")
+        binding.email.text = arguments?.getString("email")
+        binding.password.text = arguments?.getString("password")
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            binding.usernameRevision.id -> {
+                binding.revisionLinear.visibility = View.VISIBLE
+                binding.revisionText.text = "유저 이름 수정"
+                binding.editRevision.hint = "새 유저 이름을 입력하세요."
+            }
+            binding.emailRevision.id -> {
+                binding.revisionLinear.visibility = View.VISIBLE
+                binding.revisionText.text = "이메일 수정"
+                binding.editRevision.hint = "새 이메일을 입력하세요."
+            }
+            binding.passwordRevision.id -> {
+                binding.revisionLinear.visibility = View.VISIBLE
+                binding.revisionText.text = "비밀번호 수정"
+                binding.editRevision.hint = "새 비밀번호를 입력하세요."
+            }
+            binding.saveRevision1.id -> {
+                if (binding.editRevision.text.isNotEmpty()) {
+                    when(binding.editRevision.hint) {
+                        "새 유저 이름을 입력하세요." -> {
+                            binding.name.text = binding.editRevision.text.toString()
+                            binding.saveRevision2.visibility = View.VISIBLE
+                        }
+                        "새 이메일을 입력하세요." -> {
+                            binding.email.text = binding.editRevision.text.toString()
+                            binding.saveRevision2.visibility = View.VISIBLE
+                        }
+                        "새 비밀번호를 입력하세요." -> {
+                            binding.password.text = binding.editRevision.text.toString()
+                            binding.saveRevision2.visibility = View.VISIBLE
+                        }
+                    }
+                } else {
+                    Log.d("FAIL", "edit text is empty...")
+                }
+            }
+            binding.saveRevision2.id -> {
+                val id = arguments?.getInt("id")
+                nbViewModel.revisionLogic(id!!, binding.name.text.toString(), binding.email.text.toString(), binding.password.text.toString())
+                nbViewModel.userRevisionApiCallResult.observe(viewLifecycleOwner) {
+                    Log.d("SUCCESS", "revisionLogic it: $it")
+                }
+            }
+        }
+    }
+
+    private fun viewSetting() {
+        binding.apply {
+            usernameRevision.setOnClickListener(this@UserInfoFragment)
+            emailRevision.setOnClickListener(this@UserInfoFragment)
+            passwordRevision.setOnClickListener(this@UserInfoFragment)
+            saveRevision1.setOnClickListener(this@UserInfoFragment)
+            saveRevision2.setOnClickListener(this@UserInfoFragment)
+        }
+    }
+}
