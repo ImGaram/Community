b432467a133eabcdef6048c661fa81ee6b77f851
imgaram <s21049@gsm.hs.kr>
2022-05-28 21:40:12 +0900

    django community 12-9: 자유게시판 포스팅 mapper

---
+++ data/src/main/java/com/example/data/mapper/FreeBoardMapper.kt
@@ -0,0 +1,27 @@
+package com.example.data.mapper
+
+import com.example.data.entity.freeboard.response.AddFreeBoardResponse
+import com.example.domain.model.freeboard.DomainAddFreeBoardResponse
+
+object FreeBoardMapper {
+//    data -> domain
+    fun toDomainAddFreeBoardResponse(
+        dataResponse: AddFreeBoardResponse?
+    ): DomainAddFreeBoardResponse? {
+        return if (dataResponse != null) {
+            DomainAddFreeBoardResponse(
+                id = dataResponse.id,
+                title = dataResponse.title,
+                context = dataResponse.context,
+                img1 = dataResponse.img1,
+                img2 = dataResponse.img2,
+                img3 = dataResponse.img3,
+                img4 = dataResponse.img4,
+                img5 = dataResponse.img5,
+                createUser = dataResponse.createUser,
+                createDate = dataResponse.createDate,
+                correctionDate = dataResponse.correctionDate
+            )
+        } else dataResponse
+    }
+}
