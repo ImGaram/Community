fa3a2844ba25084fe574f7c20f615c0f075d9256
imgaram <s21049@gsm.hs.kr>
2022-05-28 21:39:53 +0900

    django community 12-8: 자유게시판 포스팅 api service

---
+++ data/src/main/java/com/example/data/api/freeboard/FreeAddPostService.kt
@@ -0,0 +1,12 @@
+package com.example.data.api.freeboard
+
+import com.example.data.entity.freeboard.request.AddFreeBoardRequest
+import com.example.data.entity.freeboard.response.AddFreeBoardResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface FreeAddPostService {
+    @POST("/FBN/CRE/")
+    suspend fun createPost(@Body body: AddFreeBoardRequest): Response<AddFreeBoardResponse>
+}
