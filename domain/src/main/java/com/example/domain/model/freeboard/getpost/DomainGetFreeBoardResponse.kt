3e39045576eb01063e1c02fad1b3d0c2a094d546
imgaram <s21049@gsm.hs.kr>
2022-05-30 20:10:05 +0900

    django community 13-1: 포스트 가져오기 api response 파일

---
+++ domain/src/main/java/com/example/domain/model/freeboard/getpost/DomainGetFreeBoardResponse.kt
@@ -0,0 +1,15 @@
+package com.example.domain.model.freeboard.getpost
+
+interface DomainGetFreeBoardResponse {
+    val _id: Int
+    val _title: String
+    val _context:String
+    val _img1:String
+    val _img2:String
+    val _img3:String
+    val _img4:String
+    val _img5:String
+    val _createUser: Int
+    val _createDate: String
+    val _correctionDate: String
+}
