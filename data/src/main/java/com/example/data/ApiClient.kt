06c3b89779efd4044a4270441eb465fbf6d46fce
imgaram <s21049@gsm.hs.kr>
2022-05-31 17:33:07 +0900

    django community 13-10: api 파일 수정

--- data/src/main/java/com/example/data/ApiClient.kt
+++ data/src/main/java/com/example/data/ApiClient.kt
@@ -3,4 +3,6 @@ package com.example.data
 object ApiClient {
     const val BASE_URL = "http://10.120.74.35:8000/"
     var resultCode: Int? = null
+//    http://192.168.137.146:8000/
+//    http://10.120.74.35:8000/
 }
