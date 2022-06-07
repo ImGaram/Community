f29be53953410306fc86644254218c3b7c2765f1
imgaram <s21049@gsm.hs.kr>
2022-06-07 22:35:59 +0900

    django community 14-9: post 수정 activity(생성)와 xml

---
+++ presentation/src/main/java/com/example/presentation/view/freeboard/info/PostModifyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.presentation.view.freeboard.info
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.presentation.R
+
+class PostModifyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_modify)
+    }
+}
