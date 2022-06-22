e896fb285ff947c41614607d6c7300b44ebe7177
imgaram <s21049@gsm.hs.kr>
2022-06-22 22:19:06 +0900

    django community 17-3: add comment use case

---
+++ domain/src/main/java/com/example/domain/usecase/freeboard/comment/AddCommentUseCase.kt
@@ -0,0 +1,26 @@
+package com.example.domain.usecase.freeboard.comment
+
+import com.example.domain.repository.freeboard.comment.AddCommentRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+
+class AddCommentUseCase(
+    private val repository: AddCommentRepository
+) {
+    operator fun invoke(
+        context: String,
+        createUserId: Int,
+        commentPost: Int,
+        scope: CoroutineScope,
+        onResult: (Int?) -> Unit = {}
+    ) {
+        scope.launch(Dispatchers.Main) {
+            val deferred = async(Dispatchers.IO) {
+                repository.createComment(context, createUserId, commentPost)
+            }
+            onResult(deferred.await())
+        }
+    }
+}
