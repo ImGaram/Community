a29835294d9628197781c3789c80c82b3af0a8a5
imgaram <s21049@gsm.hs.kr>
2022-07-04 22:12:58 +0900

    django community 22-5: 스토리 게시판 게시글 가져오기 viewmodel 추가, recycler item, adapter 생성

---
+++ presentation/src/main/java/com/example/presentation/view/story/adapter/StoryListRecyclerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.presentation.view.story.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.domain.model.story.DomainBaseStoryResponse
+import com.example.presentation.databinding.RecyclerItemStoryListBinding
+
+class StoryListRecyclerAdapter(val storyList: List<DomainBaseStoryResponse>): RecyclerView.Adapter<StoryListRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = RecyclerItemStoryListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(storyList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return storyList.size
+    }
+
+    inner class ViewHolder(val binding: RecyclerItemStoryListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: DomainBaseStoryResponse) {
+            binding.storyNumber.text = item.id.toString()
+            binding.storyTitle.text = item.title
+            binding.storyCreatUser.text = item.createUser.toString()
+            binding.storyCreateDate.text = item.createDate
+        }
+    }
+}
--- presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.story.DomainBaseStoryResponse
 import com.example.domain.usecase.story.CreateStoryUseCase
+import com.example.domain.usecase.story.GetStoryUseCase
 import com.example.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,7 +13,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class StoryViewModel @Inject constructor(
-    private val createStoryUseCase: CreateStoryUseCase
+    private val createStoryUseCase: CreateStoryUseCase,
+    private val getStoryUseCase: GetStoryUseCase
 ): BaseViewModel() {
     // create story
     private val _createStory = MutableLiveData<DomainBaseStoryResponse>()
@@ -25,6 +27,17 @@ class StoryViewModel @Inject constructor(
         viewModelScope.launch {
             createStoryUseCase(title, context, createUser, viewModelScope) {
                 _createStory.value = it
+            }
+        }
+    }
+
+    // get story
+    private val _getStory = MutableLiveData<List<DomainBaseStoryResponse>>()
+    val getStory: LiveData<List<DomainBaseStoryResponse>> = _getStory
+    fun getStoryLogic() {
+        viewModelScope.launch {
+            getStoryUseCase(viewModelScope) {
+                _getStory.value = it
             }
         }
     }
