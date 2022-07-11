4b5dd1709024c1c60fc08ed36db75ddfcbc604db
imgaram <s21049@gsm.hs.kr>
2022-07-11 16:56:19 +0900

    django community 24-4: story 수정 view model 추가

--- presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
@@ -7,6 +7,7 @@ import com.example.domain.model.story.DomainBaseStoryResponse
 import com.example.domain.usecase.story.CreateStoryUseCase
 import com.example.domain.usecase.story.GetSingleStoryUseCase
 import com.example.domain.usecase.story.GetStoryUseCase
+import com.example.domain.usecase.story.ModifyStoryUseCase
 import com.example.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -16,7 +17,8 @@ import javax.inject.Inject
 class StoryViewModel @Inject constructor(
     private val createStoryUseCase: CreateStoryUseCase,
     private val getStoryUseCase: GetStoryUseCase,
-    private val getSingleStoryUseCase: GetSingleStoryUseCase
+    private val getSingleStoryUseCase: GetSingleStoryUseCase,
+    private val modifyStoryUseCase: ModifyStoryUseCase
 ): BaseViewModel() {
     // create story
     private val _createStory = MutableLiveData<DomainBaseStoryResponse>()
@@ -51,6 +53,17 @@ class StoryViewModel @Inject constructor(
         viewModelScope.launch {
             getSingleStoryUseCase(storyId, viewModelScope) {
                 _getSingleStory.value = it
+            }
+        }
+    }
+
+    // modify story
+    private val _modifyStory = MutableLiveData<DomainBaseStoryResponse>()
+    val modifyStory: LiveData<DomainBaseStoryResponse> = _modifyStory
+    fun modifyStoryLogic(story: Int, title: String, content: String, createUser: Int) {
+        viewModelScope.launch {
+            modifyStoryUseCase(story, title, content, createUser, viewModelScope) {
+                _modifyStory.value = it
             }
         }
     }
