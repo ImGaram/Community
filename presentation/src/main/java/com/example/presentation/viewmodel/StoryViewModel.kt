91d5c4b1bb969dde691f3a8372301c803cab9f59
imgaram <s21049@gsm.hs.kr>
2022-07-12 23:19:34 +0900

    django community 27-5: get comment list view model 추가

--- presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
+++ presentation/src/main/java/com/example/presentation/viewmodel/StoryViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.domain.model.story.DomainBaseStoryResponse
-import com.example.domain.model.story.DomainCommentResponse
+import com.example.domain.model.story.DomainBaseCommentResponse
 import com.example.domain.usecase.story.*
 import com.example.domain.usecase.story.comment.CreateCommentUseCase
+import com.example.domain.usecase.story.comment.GetCommentListUseCase
 import com.example.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -19,7 +20,8 @@ class StoryViewModel @Inject constructor(
     private val getSingleStoryUseCase: GetSingleStoryUseCase,
     private val modifyStoryUseCase: ModifyStoryUseCase,
     private val deleteStoryUseCase: DeleteStoryUseCase,
-    private val createCommentUseCase: CreateCommentUseCase
+    private val createCommentUseCase: CreateCommentUseCase,
+    private val getCommentListUseCase: GetCommentListUseCase
 ): BaseViewModel() {
     // create story
     private val _createStory = MutableLiveData<DomainBaseStoryResponse>()
@@ -81,12 +83,23 @@ class StoryViewModel @Inject constructor(
     }
 
     // create comment
-    private val _createComment = MutableLiveData<DomainCommentResponse>()
-    val createComment: LiveData<DomainCommentResponse> = _createComment
+    private val _createComment = MutableLiveData<DomainBaseCommentResponse>()
+    val createComment: LiveData<DomainBaseCommentResponse> = _createComment
     fun createCommentLogic(context: String, createIdUserSt: Int, commentStory: Int) {
         viewModelScope.launch {
             createCommentUseCase(context, createIdUserSt, commentStory, viewModelScope) {
                 _createComment.value = it
+            }
+        }
+    }
+
+    // get comment list
+    private val _getCommentList = MutableLiveData<List<DomainBaseCommentResponse>>()
+    val getCommentList: LiveData<List<DomainBaseCommentResponse>> = _getCommentList
+    fun getCommentListLogic(story: Int) {
+        viewModelScope.launch {
+            getCommentListUseCase(story, viewModelScope) {
+                _getCommentList.value = it
             }
         }
     }
