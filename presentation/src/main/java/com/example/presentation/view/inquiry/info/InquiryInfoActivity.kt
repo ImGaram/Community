8f3a9d30552d6595c3bd95e18fcae6566d70467b
imgaram <s21049@gsm.hs.kr>
2022-07-17 15:03:10 +0900

    django community 34-4: 문의사항 삭제 기능 구현

--- presentation/src/main/java/com/example/presentation/view/inquiry/info/InquiryInfoActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/inquiry/info/InquiryInfoActivity.kt
@@ -1,8 +1,16 @@
 package com.example.presentation.view.inquiry.info
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.Window
 import androidx.activity.viewModels
+import androidx.appcompat.widget.AppCompatButton
 import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivityInquiryInfoBinding
@@ -31,15 +39,47 @@ class InquiryInfoActivity : BaseActivity<ActivityInquiryInfoBinding>(R.layout.ac
                 startActivity(intent)
             }
             binding.inquiryDelete.id -> {
+                val delDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog_delete, null)
+                val dialogBuilder = AlertDialog.Builder(this)
+                    .setView(delDialogView)
+                val dialog = dialogBuilder.show()
+                val mDialog = Dialog(this)
+                mDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                mDialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
 
+                delDialogView.findViewById<AppCompatButton>(R.id.delete_ok_btn).setOnClickListener {
+                    deleteLogic(dialog)
+                }
+                delDialogView.findViewById<AppCompatButton>(R.id.delete_cancel_btn).setOnClickListener { dialog.dismiss() }
             }
             binding.inquiryCancelView.id -> finish()
+        }
+    }
+
+    private fun deleteLogic(dialog: AlertDialog?) {
+        inquiryViewModel.deleteInquiryLogic(intent.getIntExtra("postId", 0))
+        inquiryViewModel.deleteInquiry.observe(this) {
+            when (it) {
+                204 -> { dialog?.dismiss() }
+                404 -> {
+                    Log.d("ERROR", "deleteLogic response code: $it")
+                    dialog?.dismiss()
+                }
+                else -> {
+                    Log.d("ERROR", "deleteLogic response code: $it")
+                    dialog?.dismiss()
+                }
+            }
         }
     }
 
     private fun dataSetting() {
         inquiryViewModel.getInquiryLogic(intent.getIntExtra("postId", 0))
         inquiryViewModel.getInquiry.observe(this) {
+            if (intent.getIntExtra("userId", 0) != it.createUserId) {
+                binding.inquiryBtnLinear.visibility = View.GONE
+            }
+
             binding.inquiryInfoTitle.text = it.title
             binding.inquiryContentText.text = it.context
             binding.inquiryCreateDate.text = it.createDate
